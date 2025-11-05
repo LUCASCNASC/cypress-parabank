84a089772f15bb4adb29356153075a75389ac01a
Lucas Camargo <nascimentocamargolucas@gmail.com>
2025-11-05 15:47:47 -0300

    Creating PageObjects.

---
+++ cypress/pages/SendEmailPage.ts
@@ -0,0 +1,10 @@
+import { generateNewCustomerData, CustomerData } from '../utils/dataGenerator';
+let userData: CustomerData; 
+
+export class SendEmailPage {
+    
+    // clickRegister() {
+    //     cy.contains('Register').click();
+    // }
+
+}
