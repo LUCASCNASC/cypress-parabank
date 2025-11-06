5f894ddc130f13384f5c3f2456eb664626b0b0f8
Lucas Camargo <nascimentocamargolucas@gmail.com>
2025-11-06 15:25:48 -0300

    Desenvolvendo cenários de teste.

---
+++ cypress/pages/ForgotLoginInfoPage.ts
@@ -0,0 +1,10 @@
+import { generateNewCustomerData, CustomerData } from '../utils/dataGenerator';
+let userData: CustomerData; 
+
+export class ForgotLoginInfoPage {
+    
+    // clickRegister() {
+    //     cy.contains('Register').click();
+    // }
+
+}
