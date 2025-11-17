bfa1a8db66c2fb63a32ba28bf50ed90813f48035
Lucas Camargo <nascimentocamargolucas@gmail.com>
2025-11-17 13:35:14 -0300

    Modificando projeto.

--- cypress/pages/AboutUsPage.ts
+++
@@ -1,7 +0,0 @@
-export class AboutUsPage {
-
-    clickAboutUs() {
-        cy.get('a[href="about.htm"]').first().click()
-        cy.contains('ParaSoft Demo Website').should('be.visible');
-    }
-}
--- cypress/pages/AdminPagePage.ts
+++
@@ -1,7 +0,0 @@
-export class AdminPagePage {
-    
-    clickAdminPage() {
-        cy.contains('a', 'Admin Page').click();
-        cy.contains('Administration').should('be.visible');
-    }
-}
