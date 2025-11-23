53770174171ae34bae36bce87ae4e3374fdd2a03
Lucas Camargo <nascimentocamargolucas@gmail.com>
2025-11-23 12:59:58 -0300

    Ajustando arquivos de teste.

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   projectId: "hnqg5e",
 
   e2e: {
-    baseUrl: process.env.BASE_URL,   // Ex: https://meusite.com
+    baseUrl: process.env.BASE_URL,
     defaultCommandTimeout: 8000,
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: "cypress/support/e2e.ts",
